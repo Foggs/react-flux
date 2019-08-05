3a9e7995a438d1b940d470fbe483b60401c8e7c1
= <robfoggo@fcb.com>
2019-08-05 11:57:15 -0400

    testing routes, cookies enabled through passport

--- index.js
+++ index.js
@@ -1,48 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const path = require('path');
-const authRoutes = require('./routes/authRoutes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieSession = require("cookie-session")
+const passport = require("passport");
+const keys = require("./config/keys");
 
-require('./services/passport');
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.MLab_URI, { useNewUrlParser: true },
+  (err,client) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(" db connect!!!");
+});
 
 const app = express();
-// pass the app into the routes Object to attach routes 
-authRoutes(app)
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+  keys: [keys.cookieKey]
+ }))
+ app.use(passport.initialize());
+ app.use(passport.session());
 
  
-
-/* MIDDLEWARE */
-app.use(bodyParser.json());
-app.use(express.static('client/build'));
-
-
-var opts = {
-    server: {
-        socketOptions: {keepAlive: 1}
-    }
-};
-
-// mongoose.connect(keys.mongoURI, opts);
-// var conn = mongoose.connection;
-
-// conn.on('error', console.error.bind(console, 'connection error:'));  
- 
-// conn.once('open', function() {
-//   // Wait for the database connection to establish, then start the app.                         
-// });
-
-
-/* ROUTE HANDLERS */
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve('client', 'build', 'index.html'));
-// });
-
-
-
-// app.get('/auth/google', (req, res) => {
-//     res.sendFile(path.resolve('client', 'build', 'index.html'));
-// });
+// pass the app into the routes Object to attach routes
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 // get environment variable PORT if available
--- services/passport.js
+++ services/passport.js
@@ -1,17 +1,39 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null,user);
+  });
+});
 
 passport.use(
-    new GoogleStrategy({
-        clientID: keys.GOOGLE_CLIENT_ID,
-        clientSecret: keys.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback"
-      }, 
-      (accessToken, refreshToken, profile, done) => {
-        console.log('profile', profile);
-        console.log('accessToken', accessToken);
-        console.log('refreshToken', refreshToken);
-      }
-));
-
+  new GoogleStrategy(
+    {
+      clientID: keys.GOOGLE_CLIENT_ID,
+      clientSecret: keys.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:5000/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("accessToken:", accessToken);
+      // search collection for profile.id, returns Promise
+      User.findOne({ googleId: project.id }).then(existingUser => {
+        // we have a record wit the ID
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          // no record found create new User
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
